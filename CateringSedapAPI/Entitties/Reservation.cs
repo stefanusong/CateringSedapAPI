a5a13d93f3344100ec0965fe917a33c9ebb2c999
Stefanus Ong <stefanusoms@gmail.com>
2022-04-12 10:58:41 +0700

    Add project files.

---
+++ CateringSedapAPI/Entitties/Reservation.cs
@@ -0,0 +1,22 @@
+﻿namespace CateringSedapAPI.Entitties
+{
+    public class Reservation
+    {
+        private Guid Id { get; set; }
+        private ReservationStatus Status { get; set; }
+        private DateTime ReservationDate { get; set; }
+        
+    }
+
+
+    public enum ReservationStatus
+    {
+        Received = 1,
+        Processed,
+        Accepted,
+        WaitingForPayment,
+        Rejected,
+        OnDelivery,
+        Delivered
+    }
+}
---
+++ CateringSedapAPI/Entitties/User.cs
@@ -0,0 +1,10 @@
+﻿namespace CateringSedapAPI.Entitties
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+        public string Username { get; set; }
+        public string Name { get; set; }
+
+    }
+}
