7c47c242fefc6552230ed8ca52e2359c9f85c5a7
Ferryansen <kwanan57@gmail.com>
2022-05-24 19:02:51 +0700

    added payment controller

--- CateringSedapAPI/Controllers/PaymentController.cs
+++ CateringSedapAPI/Controllers/PaymentController.cs
@@ -1,5 +1,4 @@
-﻿using CateringSedapAPI.Context;
-using CateringSedapAPI.Dto;
+﻿using CateringSedapAPI.Dto;
 using CateringSedapAPI.Entitties;
 using CateringSedapAPI.Helpers;
 using CateringSedapAPI.Services;
@@ -21,5 +20,111 @@ namespace CateringSedapAPI.Controllers
             _paymentService = paymentService;
             _responseHelper = responseHelper;
         }
+
+        [HttpGet]
+        public async Task<IActionResult> GetPayments()
+        {
+            try
+            {
+                var payments = await _paymentService.GetPayments();
+                if (payments.Any())
+                {
+                    return Ok(_responseHelper.GetSuccessResponse("Payment retrieved successfully", payments));
+                }
+                else
+                {
+                    return NotFound(_responseHelper.GetSuccessResponse("No payment found", new { }));
+                }
+            }
+            catch (Exception e)
+            {
+                return BadRequest(_responseHelper.GetErrorResponse(e.Message));
+            }
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetPayment(Guid id)
+        {
+            try
+            {
+                var payment = await _paymentService.GetPayment(id);
+                if (payment != null)
+                {
+                    return Ok(_responseHelper.GetSuccessResponse("Payment retrieved successfully", payment));
+                }
+                else
+                {
+                    return NotFound(_responseHelper.GetSuccessResponse("Payment not found", new { }));
+                }
+            }
+            catch (Exception e)
+            {
+                return BadRequest(_responseHelper.GetErrorResponse(e.Message));
+            }
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreatePayment(PaymentDto paymentDetail)
+        {
+            try
+            {
+                var payment = await _paymentService.CreatePayment(paymentDetail);
+                if (payment != null)
+                {
+                    return Ok(_responseHelper.GetSuccessResponse("Payment created successfully", payment));
+                }
+                else
+                {
+                    return BadRequest(_responseHelper.GetErrorResponse("Payment failed to be created"));
+                }
+            }
+            catch(Exception e)
+            {
+                return BadRequest(_responseHelper.GetErrorResponse(e.Message));
+            }
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdatePayment (Guid id, Payment.PaymentStatus status)
+        {
+            try
+            {
+                var payment = await _paymentService.UpdatePaymentStatus(id, status);
+                if (payment)
+                {
+                    return Ok(_responseHelper.GetSuccessResponse("Payment status updated successfully", payment));
+                }
+                else
+                {
+                    return BadRequest(_responseHelper.GetErrorResponse("Payment status not updated"));
+                }
+            }
+            catch (Exception e)
+            {
+                return BadRequest (_responseHelper.GetErrorResponse(e.Message));
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeletePayment(Guid id)
+        {
+            try
+            {
+                var payment = await _paymentService.CancelPayment(id);
+                if (payment)
+                {
+                    return Ok(_responseHelper.GetSuccessResponse("Payment cancelled successfully", payment));
+                }
+                else
+                {
+                    return BadRequest(_responseHelper.GetErrorResponse("Payment not cancelled"));
+                }
+            }
+            catch (Exception e)
+            {
+                return BadRequest(_responseHelper.GetErrorResponse(e.Message));
+            }
+        }
+
     }
 }
