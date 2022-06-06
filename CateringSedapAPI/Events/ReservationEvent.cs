db010b0b544bc36449e14a1497fcbfb0d2764fc7
Ferryansen <kwanan57@gmail.com>
2022-06-06 22:13:19 +0700

    added aggregates (?)

--- CateringSedapAPI/Events/ReservationEvent.cs
+++ CateringSedapAPI/Events/ReservationEvent.cs
@@ -1,6 +1,12 @@
-﻿namespace CateringSedapAPI.Events
+﻿using CateringSedapAPI.Entitties;
+
+namespace CateringSedapAPI.Events
 {
-    public interface ReservationEvent
+    public interface IReservationEvent
     {
     }
+
+    public record ReservationCreated(Guid id, ReservationStatus Status, DateTime DateTime) : IReservationEvent;
+
+    public record ReservationUpdated(Guid id, ReservationStatus Status, DateTime DateTime) : IReservationEvent;
 }
